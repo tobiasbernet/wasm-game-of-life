790d2acc1f7d19b7749b4deb2656dd03db8ce623
Tobias Bernet <tobias.bernet@shopviu.com>
2020-03-24 11:42:31 +0100

    Create a random univers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate js_sys;
+
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
@@ -75,7 +77,7 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                if js_sys::Math::random() < 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
