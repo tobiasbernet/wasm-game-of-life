d70739759878e0280b40f1a7d43cb4b25157c43e
Tobias Bernet <tobias.bernet@shopviu.com>
2020-03-27 01:19:44 +0100

    Setup gcloud server

---
+++ www/server.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
+
+app.get('/', (req, res) => {
+ res.render('index');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+ console.log(`App listening on port ${PORT}`);
+ console.log('Press Ctrl+C to quit.');
+});
