c10415bdbf686a4aea645ef2f44fae9e55758ed7
Tobias Bernet <tobias.bernet@shopviu.com>
2020-03-27 01:19:59 +0100

    Styling

--- www/index.js
+++ www/index.js
@@ -1,19 +1,26 @@
-import { Universe, Cell } from "wasm-game-of-life";
-import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+import {
+  Universe,
+  Cell
+} from "wasm-game-of-life";
+import {
+  memory
+} from "wasm-game-of-life/wasm_game_of_life_bg";
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
+const ALIVE_COLOR = "#E0E5EC";
 
 // Construct the universe, and get its width and height.
-const universe = Universe.new();
+var universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
 const canvas = document.getElementById("game-of-life-canvas");
+const start = document.getElementById("start");
+
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
@@ -40,7 +47,7 @@ const drawGrid = () => {
 
   // Horizontal lines.
   for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
 
@@ -61,9 +68,9 @@ const drawCells = () => {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOR
-        : ALIVE_COLOR;
+      ctx.fillStyle = cells[idx] === Cell.Dead ?
+        DEAD_COLOR :
+        ALIVE_COLOR;
 
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
@@ -79,4 +86,16 @@ const drawCells = () => {
 
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+function initGame() {
+  if (start.value == "true") {
+    drawGrid();
+    drawCells();
+    universe = Universe.new();
+  } else {
+    start.value = "true";
+    start.firstChild.data = "Reload Universe";
+    requestAnimationFrame(renderLoop);
+  }
+}
+
+start.addEventListener("click", initGame, false)
